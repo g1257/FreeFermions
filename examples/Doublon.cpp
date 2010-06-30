f9469ae24b40bfb860ef0566efaeb6e07ba3fb5f
g1257 <gz1@ornl.gov>
2010-06-30 13:05:22 -0400

    examples: Doublon added

--- examples/Doublon.cpp
+++ examples/Doublon.cpp
@@ -22,12 +22,9 @@ typedef DiagonalOperator<EtoTheIhTimeType> DiagonalOperatorType;
 typedef ObservableLibrary<EngineType> ObservableLibraryType;
 
 
-FieldType calcSuperDensity(size_t site, size_t site2,HilbertVectorType gs,const EngineType& engine,const ObservableLibraryType& library)
+FieldType calcSuperDensity(size_t site, HilbertVectorType gs,const EngineType& engine,const ObservableLibraryType& library)
 {
 
-	HilbertVectorType savedVector = engine.newState();
-	FieldType savedValue = 0;
-	FieldType sum = 0;
 	HilbertVectorType tmpV = engine.newState();
 	
 	for (size_t sigma = 0;sigma<2;sigma++) {
@@ -41,32 +38,14 @@ FieldType calcSuperDensity(size_t site, size_t site2,HilbertVectorType gs,const 
 		tmpV.add(phi2);
 		phi.clear();
 
-		for (size_t sigma2 = 0;sigma2 < 2;sigma2++) {
-			HilbertVectorType phi3 = engine.newState();
-			library.applyNiBarOneFlavor(phi3,phi2,site2,1-sigma2);
-			
-			FreeOperatorType myOp4 = engine.newSimpleOperator("destruction",site2,sigma2);
-			HilbertVectorType phi4 = engine.newState();
-			myOp4.apply(phi4,phi3);
-			phi3.clear();
-			
-			sum += scalarProduct(phi4,phi4);
-			if (sigma ==0 && sigma2 ==0) savedVector = phi4;
-			if (sigma ==1 && sigma2 ==1) {
-				savedValue = scalarProduct(phi4,savedVector);
-				savedVector.clear();
-			}
-		}
 	}
-	sum += 2*real(savedValue);
-	std::cerr<<"sum2="<<scalarProduct(tmpV,tmpV)<<"\n";
-	return sum;
+	return scalarProduct(tmpV,tmpV);
 }
 
 
 int main(int argc,char *argv[])
 {
-	if (argc!=6) throw std::runtime_error("Needs 6 arguments\n");
+	if (argc!=5) throw std::runtime_error("Needs 5 arguments\n");
 	size_t n = 16; // 16 sites
 	size_t dof = 2; // spin up and down
 	bool isPeriodic = false; 
@@ -86,20 +65,20 @@ int main(int argc,char *argv[])
 	HilbertVectorType gs = engine.newGroundState(ne);
 	
 	size_t site = atoi(argv[1]);
-	size_t site2 = atoi(argv[2]);
-	size_t site3 = atoi(argv[3]);
+	//size_t site2 = atoi(argv[2]);
+	size_t site3 = atoi(argv[2]);
 	size_t sigma3 = 0;
 
-	FieldType superdensity = calcSuperDensity(site,site2,gs,engine,library);
+	FieldType superdensity = calcSuperDensity(site,gs,engine,library);
 	std::cout<<"superdensity="<<superdensity<<"\n";
 	
-	for (size_t it=0;it<size_t(atoi(argv[4]));it++) {
-		RealType time = it * atof(argv[5]);
+	for (size_t it=0;it<size_t(atoi(argv[3]));it++) {
+		RealType time = it * atof(argv[4]);
 		EtoTheIhTimeType eih(time,engine);
 		DiagonalOperatorType eihOp(eih);
 				
-		HilbertVectorType savedVector = engine.newState(verbose);
-		FieldType savedValue = 0;
+		//HilbertVectorType savedVector = engine.newState(verbose);
+		//FieldType savedValue = 0;
 		FieldType sum = 0;
 		for (size_t sigma = 0;sigma<2;sigma++) {
 			HilbertVectorType phi = engine.newState();
@@ -110,19 +89,19 @@ int main(int argc,char *argv[])
 			myOp2.apply(phi2,phi);
 			phi.clear();
 			
-			for (size_t sigma2 = 0;sigma2 < 2;sigma2++) {
-				HilbertVectorType phi3 = engine.newState();
-				library.applyNiBarOneFlavor(phi3,phi2,site2,1-sigma2);
+			//for (size_t sigma2 = 0;sigma2 < 2;sigma2++) {
+				//HilbertVectorType phi3 = engine.newState();
+				//library.applyNiBarOneFlavor(phi3,phi2,site2,1-sigma2);
 				
-				FreeOperatorType myOp4 = engine.newSimpleOperator("destruction",site2,sigma2);
-				HilbertVectorType phi4 = engine.newState(verbose);
-				myOp4.apply(phi4,phi3);
-				phi3.clear();
+				//FreeOperatorType myOp4 = engine.newSimpleOperator("destruction",site2,sigma2);
+				//HilbertVectorType phi4 = engine.newState(verbose);
+				//myOp4.apply(phi4,phi3);
+				//phi3.clear();
 				
 				if (verbose) std::cerr<<"Applying exp(iHt)\n";
 				HilbertVectorType phi5 = engine.newState(verbose);
-				eihOp.apply(phi5,phi4);
-				phi4.clear();
+				eihOp.apply(phi5,phi2);
+				//phi4.clear();
 				
 				if (verbose) std::cerr<<"Applying c_p\n";
 				FreeOperatorType myOp6 = engine.newSimpleOperator("destruction",site3,sigma3);
@@ -130,18 +109,18 @@ int main(int argc,char *argv[])
 				myOp6.apply(phi6,phi5);
 				phi5.clear();
 				
-				if (verbose) std::cerr<<"Adding "<<sigma<<" "<<sigma2<<" "<<it<<"\n";
+				if (verbose) std::cerr<<"Adding "<<sigma<<" "<<it<<"\n";
 				sum += scalarProduct(phi6,phi6);
 				if (verbose) std::cerr<<"Done with scalar product\n";
-				if (sigma ==0 && sigma2 ==0) savedVector = phi6;
-				if (sigma ==1 && sigma2 ==1) {
-					savedValue = scalarProduct(phi6,savedVector);
-					savedVector.clear();
-				}
+				//if (sigma ==0 && sigma2 ==0) savedVector = phi6;
+				//if (sigma ==1 && sigma2 ==1) {
+				//	savedValue = scalarProduct(phi6,savedVector);
+				//	savedVector.clear();
+				//}
 				//timeVector.add(phi6);
-			}
+			//}
 		}
-		sum += 2*real(savedValue);
+		//sum += 2*real(savedValue);
 		std::cout<<site3<<" "<<sum<<"\n";
 	}
 }
