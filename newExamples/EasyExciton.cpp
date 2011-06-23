eb1d622f06f327c752311b444debb5c6fdb2ad80
g1257 <gz1@ornl.gov>
2011-06-23 15:02:20 -0400

    added missing files

---
+++ newExamples/EasyExciton.cpp
@@ -0,0 +1,74 @@
+
+
+// SAmple of how to use FreeFermions core engine to calculate
+// <c^\dagger_i c_j >
+#include <cstdlib>
+#include "Engine.h"
+#include "GeometryLibrary.h"
+#include "ConcurrencySerial.h"
+#include "TypeToString.h"
+#include "CreationOrDestructionOp.h"
+#include "HilbertState.h"
+#include "EtoTheIhTime.h"
+#include "DiagonalOperator.h"
+
+typedef double RealType;
+typedef std::complex<double> ComplexType;
+typedef ComplexType FieldType;
+typedef PsimagLite::ConcurrencySerial<RealType> ConcurrencyType;
+typedef PsimagLite::Matrix<RealType> MatrixType;
+typedef FreeFermions::Engine<RealType,FieldType,ConcurrencyType> EngineType;
+typedef FreeFermions::CreationOrDestructionOp<EngineType> OperatorType;
+typedef FreeFermions::GeometryLibrary<MatrixType> GeometryLibraryType;
+typedef FreeFermions::EToTheIhTime<EngineType> EtoTheIhTimeType;
+typedef FreeFermions::DiagonalOperator<EtoTheIhTimeType> DiagonalOperatorType;
+typedef FreeFermions::HilbertState<OperatorType,DiagonalOperatorType> HilbertStateType;
+
+int main(int argc,char *argv[])
+{
+	if (argc!=6) throw std::runtime_error("Needs 6 arguments\n");
+	size_t n = 4;
+	size_t electronsUp = 2;
+	size_t dof = 2; // spin up and down
+	MatrixType t(n,n);
+	
+	/*GeometryLibraryType geometry(n,GeometryLibraryType::LADDER);
+	geometry.setGeometry(t,2);
+	for (size_t ii=0;ii<n;ii+=2) 
+		t(ii,ii+1) = t(ii+1,ii) = 0.5;
+	*/
+	GeometryLibraryType geometry(n,GeometryLibraryType::CHAIN);
+	geometry.setGeometry(t);
+	std::cerr<<t;
+	
+	ConcurrencyType concurrency(argc,argv);
+	EngineType engine(t,concurrency,dof,true);
+
+	std::vector<size_t> ne(dof,electronsUp); // 8 up and 8 down
+	bool debug = false;
+	HilbertStateType gs(engine.size(),ne[0],debug);
+
+	size_t sigma =0;
+	size_t site = atoi(argv[1]);
+	OperatorType myOp(engine,OperatorType::DESTRUCTION,site,sigma);
+	HilbertStateType phi2 = gs;
+	myOp.applyTo(phi2);
+	
+//	FieldType density = scalarProduct(phi2,phi2);
+//	std::cerr<<"density="<<density<<"\n";
+	
+	size_t site2=atoi(argv[2]);
+	std::cout<<"#site="<<site<<"\n";
+	std::cout<<"#site2="<<site2<<"\n";
+	for (size_t it = 0; it<size_t(atoi(argv[3])); it++) {
+		RealType time = it * atof(argv[4]) + atof(argv[5]);
+		EtoTheIhTimeType eih(time,engine,0);
+		DiagonalOperatorType eihOp(eih);
+		HilbertStateType phi = gs;
+		myOp.applyTo(phi);
+		eihOp.applyTo(phi);
+		OperatorType myOp2(engine,OperatorType::DESTRUCTION,site2,sigma);
+		myOp2.applyTo(phi);
+		std::cout<<time<<" "<<scalarProduct(phi,phi)<<"\n";
+	}
+}
---
+++ src/FermionFactor.h
@@ -0,0 +1,168 @@
+// BEGIN LICENSE BLOCK
+/*
+Copyright (c) 2009 , UT-Battelle, LLC
+All rights reserved
+
+[DMRG++, Version 2.0.0]
+[by G.A., Oak Ridge National Laboratory]
+
+UT Battelle Open Source Software License 11242008
+
+OPEN SOURCE LICENSE
+
+Subject to the conditions of this License, each
+contributor to this software hereby grants, free of
+charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), a
+perpetual, worldwide, non-exclusive, no-charge,
+royalty-free, irrevocable copyright license to use, copy,
+modify, merge, publish, distribute, and/or sublicense
+copies of the Software.
+
+1. Redistributions of Software must retain the above
+copyright and license notices, this list of conditions,
+and the following disclaimer.  Changes or modifications
+to, or derivative works of, the Software should be noted
+with comments and the contributor and organization's
+name.
+
+2. Neither the names of UT-Battelle, LLC or the
+Department of Energy nor the names of the Software
+contributors may be used to endorse or promote products
+derived from this software without specific prior written
+permission of UT-Battelle.
+
+3. The software and the end-user documentation included
+with the redistribution, with or without modification,
+must include the following acknowledgment:
+
+"This product includes software produced by UT-Battelle,
+LLC under Contract No. DE-AC05-00OR22725  with the
+Department of Energy."
+ 
+*********************************************************
+DISCLAIMER
+
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER, CONTRIBUTORS, UNITED STATES GOVERNMENT,
+OR THE UNITED STATES DEPARTMENT OF ENERGY BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED
+STATES DEPARTMENT OF ENERGY, NOR THE COPYRIGHT OWNER, NOR
+ANY OF THEIR EMPLOYEES, REPRESENTS THAT THE USE OF ANY
+INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS
+DISCLOSED WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+
+*********************************************************
+
+
+*/
+// END LICENSE BLOCK
+/** \ingroup DMRG */
+/*@{*/
+
+/*! \file FermionFactor.h
+ *
+ * Raw computations for a free Hubbard model
+ *
+ */
+#ifndef FERMION_FACTOR_H
+#define FERMION_FACTOR_H
+
+#include "Complex.h" // in PsimagLite
+#include "FreeOperators.h" // in PsimagLite
+
+
+namespace FreeFermions {
+	
+
+template<typename OperatorType,typename OpPointerType>
+	class FermionFactor {
+	public:
+		typedef typename OperatorType::RealType RealType;
+		//typedef typename OperatorType::FieldType FieldType;
+		typedef FreeOperators<OperatorType,OpPointerType> FreeOperatorsType;
+
+		enum {CREATION = OperatorType::CREATION,
+		       DESTRUCTION = OperatorType::DESTRUCTION
+		};
+
+		FermionFactor(FreeOperatorsType& freeOps,size_t ne)
+		: value_(1)
+		{
+			if (freeOps()==0) {
+				value_ = 0;
+				return;
+			}
+			freeOps.removeNonCsOrDs();
+			freeOps.reverse();
+			pairUp(ne,freeOps);
+		}
+
+		RealType operator()()
+		{
+			return value_;
+		}
+
+	private:
+
+		void pairUp(size_t ne,FreeOperatorsType& freeOps)
+		{
+			// pair up daggers with non-daggers
+			value_ = 1;
+			while(freeOps.size()>0) {
+				if (freeOps.notCreationOrDestruction(freeOps[0].type))
+					continue;
+				// take first operator's lambda:
+				size_t thisLambda = freeOps[0].lambda;
+				// find next operator with same lambda:
+				size_t x = freeOps.findOpGivenLambda(thisLambda,1);
+				// if types are equal then result is zero, and we're done:
+				if (freeOps[0].type == freeOps[x].type) {
+					value_ = 0;
+					return;
+				}
+				// then we produce a sign, and either an occupation
+				// or an anti-occupation
+				// let's deal with the (anti)occupation first:
+
+				bool nNormal = (freeOps[0].type == CREATION) ? true : false;
+				if (nNormal && thisLambda>=ne) {
+					value_=0;
+					return;
+				}
+				if (!nNormal && thisLambda<ne) {
+					value_=0;
+					return;
+				}
+				// now let's deail with the sign
+				x++;
+				int f = (x&1) ? -1 : 1;
+				value_ *= f;
+
+				// finally, we remove the pair
+				freeOps.removePair(thisLambda);
+			}
+		}
+
+		RealType value_;
+	}; // FermionFactor
+	
+
+} // namespace Dmrg 
+
+/*@}*/
+#endif // FERMION_FACTOR_H
