62a219dfc384de90b2db3fdc3e4332e1064e52e6
g1257 <gz1@ornl.gov>
2014-04-17 17:08:14 -0400

    dynamics

--- examples/dynamics.cpp
+++ examples/dynamics.cpp
@@ -1,5 +1,3 @@
-
-
 // SAmple of how to use FreeFermions core engine to calculate
 // <A_i 1/(z-H) A_j>
 #include <cstdlib>
@@ -70,8 +68,8 @@ void setMyGeometry(GeometryParamsType& geometryParams,const PsimagLite::Vector<P
 	}
 
 	if (vstr.size()!=3) {
-			usage("setMyGeometry");
-			throw std::runtime_error("setMyGeometry: usage is: -g {feas | ktwoniffour} leg filename\n");
+		usage("setMyGeometry");
+		throw std::runtime_error("setMyGeometry: usage is: -g {feas | ktwoniffour} leg filename\n");
 	}
 
 	geometryParams.filename = vstr[2];
@@ -91,10 +89,10 @@ int main(int argc,char *argv[])
 {
 	int opt;
 	PsimagLite::String file("");
-	size_t total=0;
+	SizeType total=0;
 	RealType offset = 0;
 	RealType step = 0;
-	size_t dynType = DYN_TYPE_0;
+	SizeType dynType = DYN_TYPE_0;
 
 	while ((opt = getopt(argc, argv, "f:t:o:i:d")) != -1) {
 		switch (opt) {
@@ -127,54 +125,59 @@ int main(int argc,char *argv[])
 	InputNgType::Readable io(ioWriteable);
 
 	GeometryParamsType geometryParams(io);
-	size_t electronsUp = GeometryParamsType::readElectrons(io,geometryParams.sites);
-	PsimagLite::Vector<size_t>::Type sites;
+	SizeType electronsUp = GeometryParamsType::readElectrons(io,geometryParams.sites);
+	PsimagLite::Vector<SizeType>::Type sites;
 	GeometryParamsType::readVector(sites,file,"TSPSites");
 
-	size_t dof = 1; // spinless
+	SizeType dof = 1; // spinless
 
 	GeometryLibraryType geometry(geometryParams);
 
 	std::cerr<<geometry;
-	
+
 	SizeType npthreads = 1;
 	ConcurrencyType concurrency(&argc,&argv,npthreads);
 	EngineType engine(geometry.matrix(),dof,true);
 
-	PsimagLite::Vector<size_t>::Type ne(dof,electronsUp); // 8 up and 8 down
+	PsimagLite::Vector<SizeType>::Type ne(dof,electronsUp); // 8 up and 8 down
 	bool debug = false;
 	HilbertStateType gs(engine,ne,debug);
 
 	RealType Eg = 0;
-	for (size_t i=0;i<ne[0];i++) Eg += engine.eigenvalue(i);
+	for (SizeType i=0;i<ne[0];i++) Eg += engine.eigenvalue(i);
 	std::cerr<<"Energy="<<dof*Eg<<"\n";
 
-	size_t sigma =0;
+	SizeType sigma =0;
 	OpNormalFactoryType opNormalFactory(engine);
-	size_t creatOrDest = (dynType == DYN_TYPE_1) ? OperatorType::CREATION : OperatorType::DESTRUCTION;
-	OperatorType& myOp = opNormalFactory(creatOrDest,sites[0],sigma);
+	SizeType creatOrDest = (dynType == DYN_TYPE_1) ? OperatorType::CREATION : OperatorType::DESTRUCTION;
 
-	HilbertStateType phi2 = gs;
-	myOp.applyTo(phi2);
-	
-	FieldType density = scalarProduct(phi2,phi2);
+	OperatorType& opKet = opNormalFactory(creatOrDest,sites[0],sigma);
+	HilbertStateType phiKet = gs;
+	opKet.applyTo(phiKet);
+
+	OperatorType& opBra = opNormalFactory(creatOrDest,sites[1],sigma);
+	HilbertStateType phiBra = gs;
+	opBra.applyTo(phiBra);
+
+	FieldType density = scalarProduct(phiBra,phiKet);
 	std::cerr<<"density="<<density<<"\n";
-	
+
 	std::cout<<"#site="<<sites[0]<<"\n";
 	std::cout<<"#site2="<<sites[1]<<"\n";
 
 	RealType epsilon = 1e-2;
-	for (size_t it = 0; it<total; it++) {
+	for (SizeType it = 0; it<total; it++) {
 		OpDiagonalFactoryType opDiagonalFactory(engine);
 		RealType omega = it * step + offset;
 		ComplexType z = ComplexType(omega,epsilon);
 		int sign = (dynType== DYN_TYPE_1) ? -1 : 1;
 		OneOverZminusHType eih(z,sign,Eg,engine);
 		DiagonalOperatorType& eihOp = opDiagonalFactory(eih);
-		HilbertStateType phi3 = phi2;
+		HilbertStateType phi3 = phiKet;
 		eihOp.applyTo(phi3);
-		FieldType tmpC = scalarProduct(phi2,phi3);
+		FieldType tmpC = scalarProduct(phiBra,phi3);
 
 		std::cout<<omega<<" "<<std::imag(tmpC)<<" "<<std::real(tmpC)<<"\n";
 	}
 }
+
