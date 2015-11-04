84dda5ff81e9dff782390662f3314baed5a22e99
g1257 <gz1@ornl.gov>
2015-11-04 17:22:07 -0500

    added s(q,omega) script

---
+++ examples/sqOmega.cpp
@@ -0,0 +1,251 @@
+// SAmple of how to use FreeFermions core engine to calculate
+// <A_i 1/(z-H) A_j>
+#include <cstdlib>
+#include "Engine.h"
+#include "GeometryLibrary.h"
+#include "TypeToString.h"
+#include "CreationOrDestructionOp.h"
+#include "HilbertState.h"
+#include "OneOverZminusH.h"
+#include "DiagonalOperator.h"
+#include "Tokenizer.h"
+#include "GeometryParameters.h"
+#include "Concurrency.h"
+#include "InputNg.h"
+#include "InputCheck.h"
+#include "LibraryOperator.h"
+
+typedef double RealType;
+typedef std::complex<double> ComplexType;
+typedef ComplexType FieldType;
+typedef PsimagLite::Concurrency ConcurrencyType;
+typedef PsimagLite::Matrix<RealType> MatrixType;
+typedef PsimagLite::InputNg<FreeFermions::InputCheck> InputNgType;
+typedef FreeFermions::GeometryParameters<RealType,InputNgType::Readable>
+GeometryParamsType;
+typedef FreeFermions::GeometryLibrary<MatrixType,GeometryParamsType>
+GeometryLibraryType;
+typedef FreeFermions::Engine<RealType,FieldType> EngineType;
+typedef FreeFermions::CreationOrDestructionOp<EngineType> OperatorType;
+typedef FreeFermions::OneOverZminusH<EngineType> OneOverZminusHType;
+typedef FreeFermions::DiagonalOperator<OneOverZminusHType> DiagonalOperatorType;
+typedef FreeFermions::HilbertState<OperatorType,DiagonalOperatorType> HilbertStateType;
+typedef DiagonalOperatorType::FactoryType OpDiagonalFactoryType;
+typedef OperatorType::FactoryType OpNormalFactoryType;
+typedef FreeFermions::LibraryOperator<OperatorType> LibraryOperatorType;
+typedef LibraryOperatorType::FactoryType OpLibFactoryType;
+
+enum {DYN_TYPE_0,DYN_TYPE_1};
+
+void usage(const PsimagLite::String& thisFile)
+{
+	std::cout<<thisFile<<": USAGE IS "<<thisFile<<" ";
+	std::cout<<" -n sites -e electronsUp -g geometry,[leg,filename]\n";
+}
+
+void setMyGeometry(GeometryParamsType& geometryParams,
+                   const PsimagLite::Vector<PsimagLite::String>::Type& vstr)
+{
+	// default value
+	geometryParams.type = GeometryLibraryType::CHAIN;
+
+	if (vstr.size()<2) {
+		// assume chain
+		return;
+	}
+
+	PsimagLite::String gName = vstr[0];
+	if (gName == "chain") {
+		PsimagLite::String str("setMyGeometry: ");
+		throw std::runtime_error(str + "-g chain takes no further arguments\n");
+	}
+
+	geometryParams.leg = atoi(vstr[1].c_str());
+
+	if (gName == "ladder") {
+		if (vstr.size()!=3) {
+			usage("setMyGeometry");
+			PsimagLite::String str("setMyGeometry: usage is: ");
+			throw std::runtime_error(str + "-g ladder,leg,isPeriodic\n");
+		}
+		geometryParams.type = GeometryLibraryType::LADDER;
+		geometryParams.hopping.resize(2);
+		geometryParams.hopping[0] =  geometryParams.hopping[1]  = 1.0;
+		geometryParams.isPeriodic[GeometryParamsType::DIRECTION_Y] =
+		        (atoi(vstr[2].c_str())>0);
+		return;
+	}
+
+	if (vstr.size()!=3) {
+		usage("setMyGeometry");
+		PsimagLite::String str("setMyGeometry: usage is: ");
+		throw std::runtime_error(str + "-g {feas | ktwoniffour} leg filename\n");
+	}
+
+	geometryParams.filename = vstr[2];
+
+	if (gName == "feas") {
+		geometryParams.type = GeometryLibraryType::FEAS;
+		return;
+	}
+
+	if (gName == "kniffour") {
+		geometryParams.type = GeometryLibraryType::KTWONIFFOUR;
+		return;
+	}
+}
+
+FieldType doOneOmegaOneSitePair(const EngineType& engine,
+                                const HilbertStateType& gs,
+                                OpLibFactoryType& opLibFactory,
+                                SizeType site0,
+                                SizeType site1,
+                                RealType Eg,
+                                SizeType dynType,
+                                RealType omega)
+{
+	FieldType tmpC = 0.0;
+	RealType epsilon = 1e-2;
+	for (SizeType sigma0 = 0; sigma0 < 2; ++sigma0) {
+		for (SizeType sigma1 = 0; sigma1 < 2; ++sigma1) {
+			RealType sign = (sigma0 == sigma1) ? 1.0 : -1.0;
+			RealType signForDen = (dynType== DYN_TYPE_1) ? -1.0 : 1.0;
+			LibraryOperatorType& nOpI = opLibFactory(LibraryOperatorType::N,
+			                                         site0,
+			                                         sigma0);
+
+			HilbertStateType phiKet = gs;
+			nOpI.applyTo(phiKet);
+
+			LibraryOperatorType& nOpJ = opLibFactory(LibraryOperatorType::N,
+			                                         site1,
+			                                         sigma1);
+			HilbertStateType phiBra = gs;
+			nOpJ.applyTo(phiBra);
+
+			//FieldType density = scalarProduct(phiBra,phiKet);
+			//std::cerr<<"density="<<density<<"\n";
+
+			OpDiagonalFactoryType opDiagonalFactory(engine);
+
+			ComplexType z = ComplexType(omega,epsilon);
+			//int sign = (dynType== DYN_TYPE_1) ? -1 : 1;
+			OneOverZminusHType eih(z,signForDen,Eg,engine);
+			DiagonalOperatorType& eihOp = opDiagonalFactory(eih);
+			HilbertStateType phi3 = phiKet;
+			eihOp.applyTo(phi3);
+
+			tmpC += sign*scalarProduct(phiBra,phi3);
+		}
+	}
+
+	return tmpC;
+}
+
+void doOneOmega(const EngineType& engine,
+                const HilbertStateType& gs,
+                RealType Eg,
+                SizeType totalSites,
+                SizeType centralSite,
+                SizeType dynType,
+                RealType omega)
+{
+	OpLibFactoryType opLibFactory(engine);
+	SizeType site0 = centralSite;
+	std::cout<<omega<<" ";
+	for (SizeType site1 = 0; site1 < totalSites; ++site1) {
+		FieldType val = doOneOmegaOneSitePair(engine,
+		                                      gs,
+		                                      opLibFactory,
+		                                      site0,
+		                                      site1,
+		                                      Eg,
+		                                      dynType,
+		                                      omega);
+		std::cout<<std::imag(val)<<" "<<std::real(val)<<" ";
+	}
+
+	std::cout<<"\n";
+}
+
+int main(int argc,char *argv[])
+{
+	int opt;
+	PsimagLite::String file("");
+	SizeType total=0;
+	RealType offset = 0;
+	RealType step = 0;
+	SizeType dynType = DYN_TYPE_0;
+
+	while ((opt = getopt(argc, argv, "f:t:o:i:d")) != -1) {
+		switch (opt) {
+		case 'f':
+			file=optarg;
+			break;
+		case 't':
+			total = atoi(optarg);
+			break;
+		case 'i':
+			step = atof(optarg);
+			break;
+		case 'o':
+			offset = atof(optarg);
+			break;
+		case 'd':
+			dynType = DYN_TYPE_1;
+			break;
+		default: /* '?' */
+			throw std::runtime_error("Wrong usage\n");
+		}
+	}
+
+	if (file=="") {
+		throw std::runtime_error("Wrong usage\n");
+	}
+
+	FreeFermions::InputCheck inputCheck;
+	InputNgType::Writeable ioWriteable(file,inputCheck);
+	InputNgType::Readable io(ioWriteable);
+
+	GeometryParamsType geometryParams(io);
+	SizeType electronsUp = GeometryParamsType::readElectrons(io,geometryParams.sites);
+
+	SizeType dof = 2;
+
+	GeometryLibraryType geometry(geometryParams);
+
+	std::cerr<<geometry;
+
+	SizeType npthreads = 1;
+	ConcurrencyType concurrency(&argc,&argv,npthreads);
+	EngineType engine(geometry.matrix(),dof,true);
+
+	PsimagLite::Vector<SizeType>::Type ne(dof,electronsUp);
+	bool debug = false;
+	HilbertStateType gs(engine,ne,debug);
+
+	RealType Eg = 0;
+	for (SizeType i=0;i<ne[0];i++) Eg += engine.eigenvalue(i);
+	std::cerr<<"Energy="<<dof*Eg<<"\n";
+
+	SizeType centralSite = static_cast<SizeType>(geometryParams.sites/2);
+	std::cout<<"#TotalNumberOfSites="<<geometryParams.sites<<"\n";
+	std::cout<<"#OmegaTotal="<<total<<"\n";
+	std::cout<<"#OmegaBegin="<<offset<<"\n";
+	std::cout<<"#OmegaStep="<<step<<"\n";
+	std::cout<<"#GeometryKind="<<geometryParams.geometry<<"\n";
+	std::cout<<"#TSPSites 1 "<<centralSite<<"\n";
+	std::cout<<"#############\n";
+
+	for (SizeType it = 0; it<total; it++) {
+		RealType omega = it * step + offset;
+		doOneOmega(engine,
+		           gs,
+		           Eg,
+		           geometryParams.sites,
+		           centralSite,
+		           dynType,
+		           omega);
+	}
+}
+
---
+++ examples/sqOmega.pl
@@ -0,0 +1,223 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Math::Trig;
+use OmegaUtils;
+use Getopt::Long qw(:config no_ignore_case);
+
+my $usage = "-f input [-M mForQ] [-p] [-r]\n";
+
+my ($input,$geometry,$GlobalNumberOfSites);
+my ($mForQ,$isPeriodic,$mMax,$wantsRealPart);
+my ($omega0,$omegaStep,$centralSite,$total);
+
+GetOptions('f=s' => \$input,
+           'm:i' => \$mForQ,
+           'p' => \$isPeriodic,
+           'M:i' => \$mMax,
+           'r' => \$wantsRealPart) or die "$usage\n";
+
+(defined($input)) or die "$0: USAGE: $usage\n";
+
+my $hptr = {"#OmegaBegin" => \$omega0,
+            "#OmegaTotal" => \$total,
+            "#OmegaStep" => \$omegaStep,
+            "TSPSites 1" => \$centralSite,
+            "GeometryKind" =>\$geometry,
+            "TotalNumberOfSites" => \$GlobalNumberOfSites};
+
+OmegaUtils::getLabels($hptr,$input);
+
+my @spaceValues;
+readSpace(\@spaceValues,$input);
+
+my @qvalues;
+my @omegas;
+for (my $i = 0; $i < $total; ++$i) {
+	$omegas[$i] = $omega0 + $omegaStep*$i;
+	$qvalues[$i] = procThisOmega($omegas[$i],$spaceValues[$i]);
+}
+
+printGnuplot(\@qvalues,\@omegas,$geometry);
+
+sub procThisOmega
+{
+	my ($omega,$spaceForThisOmega) = @_;
+
+	my @qValues;
+	fourier(\@qValues,$spaceForThisOmega,$geometry);
+	return \@qValues;
+}
+
+sub readSpace
+{
+	my ($space,$inFile) = @_;
+	my $counter = 0;
+
+	open(FIN,"$inFile") or die "$0: Cannot open $inFile : $!\n";
+	while(<FIN>) {
+		if (/^#/) {
+		        next;
+		}
+
+		my @temp=split;
+		my $n = scalar(@temp);
+		next unless ($n == 2*$GlobalNumberOfSites + 1);
+		my @temp2;
+		for (my $i = 1; $i < $n; ++$i) {
+			$temp2[$i-1] = $temp[$i];
+		}
+
+		$space->[$counter++] = \@temp2;
+	}
+
+	close(FIN);
+	print STDERR "$0: Read $counter omegas\n";
+}
+
+sub printGnuplot
+{
+	my ($array,$omegas,$geometry) = @_;
+
+	my $numberOfOmegas = scalar(@$omegas);
+
+	my $factor = 0;
+	my @fileIndices=(0);
+	if ($geometry eq "chain") {
+		$factor = 1.0;
+	} elsif ($geometry eq "ladder") {
+		$factor = 0.5;
+		@fileIndices=(0,1);
+	} else {
+		die "$0: Unknown geometry $geometry\n";
+	}
+
+	foreach my $fileIndex (@fileIndices) {
+		my $outFile = "outSpectrum$fileIndex.gnuplot";
+		open(FOUT,"> $outFile") or die "$0: Cannot write to $outFile : $!\n";
+
+		for (my $i = 0; $i < $numberOfOmegas; ++$i) {
+			my $omega = $omegas->[$i];
+			my $a = $array->[$i];
+			my $numberOfQs = int($factor*scalar(@$a));
+			for (my $m = 0; $m < $numberOfQs; ++$m) {
+				my $q = getQ($m,$numberOfQs);
+				my $realAndImag = $a->[$m + $fileIndex*$numberOfQs];
+				my $n2 = scalar(@$realAndImag);
+				$n2 == 2 or die "$0: Error $n2 != 2\n";
+				my $realPart = $realAndImag->[0];
+				my $imagPart = $realAndImag->[1];
+				print FOUT "$q $omega $realPart $imagPart\n";
+			}
+		}
+
+		close(FOUT);
+		print "$0: Written $outFile\n";
+	}
+}
+sub fourier
+{
+	my ($f,$v,$geometry) = @_;
+
+	if ($geometry eq "chain") {
+		return fourierChain($f,$v);
+	}
+
+	if ($geometry eq "ladder") {
+		return fourierLadder($f,$v);
+	}
+
+	die "$0: ft: undefined geometry $geometry\n";
+}
+
+sub fourierChain
+{
+	my ($f,$v) = @_;
+	my $n = int(0.5*scalar(@$v));
+	my $numberOfQs = (defined($mMax)) ? $mMax : $n;
+	for (my $m = 0; $m < $numberOfQs; ++$m) {
+		my @sum = (0,0);
+		my $q = getQ($m,$numberOfQs);
+		for (my $i = 0; $i < $n; $i++) {
+			my @temp = ($v->[2*$i],$v->[2*$i+1]);
+			my $arg = $q*($i-$centralSite);
+			my $carg = cos($arg);
+			my $sarg = sin($arg);
+			$sum[0] += $temp[0]*$carg;
+			$sum[1] += $temp[1]*$carg;
+		}
+
+		$f->[$m] = \@sum;
+	}
+}
+
+sub fourierLadder
+{
+	my ($f,$v) = @_;
+	my $n = scalar(@$v);
+	my $numberOfQs = (defined($mMax)) ? $mMax : int(0.5*$n);
+	for (my $m = 0; $m < $numberOfQs; ++$m) {
+		my $q = getQ($m,$numberOfQs);
+		my @f0 = fourierF0($v,$q);
+		my @f1 = fourierF1($v,$q);
+		for (my $x = 0; $x < 2; ++$x) {
+			my $sign = 1-2*$x;
+			my $realPart = $f0[0] + $sign*$f1[0];
+			my $imagPart = $f0[1] + $sign*$f1[1];
+			my @sum = ($realPart,$imagPart);
+			$f->[$m+$numberOfQs*$x] = \@sum;
+		}
+	}
+}
+
+sub fourierF0
+{
+	my ($v,$q) = @_;
+	my $n = scalar(@$v);
+	my @sum;
+	for (my $i = 0; $i < $n; $i+=2) {
+		my $ptr = $v->[$i];
+		my @temp = @$ptr;
+		my $arg = $q*($i-$centralSite)*0.5;
+		my $carg = cos($arg);
+		$sum[0] += $temp[0]*$carg;
+		$sum[1] += $temp[1]*$carg;
+	}
+
+	return @sum;
+}
+
+sub fourierF1
+{
+	my ($v,$q) = @_;
+	my $n = scalar(@$v);
+	my @sum;
+	for (my $i = 1; $i < $n; $i+=2) {
+		my $ptr = $v->[$i];
+		my @temp = @$ptr;
+		my $arg = $q*distanceLadder($i,$centralSite);
+		my $carg = cos($arg);
+		$sum[0] += $temp[0]*$carg;
+		$sum[1] += $temp[1]*$carg;
+	}
+
+	return @sum;
+}
+
+sub distanceLadder
+{
+	my ($ind, $jnd) = @_;
+	my $first = ($ind-1)/2;
+	my $second = $jnd/2;
+	return $first - $second;
+}
+
+sub getQ
+{
+	my ($m,$n) = @_;
+	return ($isPeriodic) ? 2.0*pi*$m/$n : pi*$m/($n+1.0);
+}
+
+
+
